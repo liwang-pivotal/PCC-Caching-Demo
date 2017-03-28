077b994b382cf970608cedd5c64338a3c11db138
Li Wang <liwang@pivotal.io>
2017-03-28 18:12:52 -0400

    use spring cloud gemfire connector

--- src/main/java/io/pivotal/DemoConfig.java
+++ src/main/java/io/pivotal/DemoConfig.java
@@ -1,68 +1,52 @@
 package io.pivotal;
 
-import java.net.URI;
-import java.util.List;
-import java.util.Properties;
+import javax.sql.DataSource;
 
-import io.pivotal.auth.EnvParser;
 import io.pivotal.domain.Customer;
+import io.pivotal.spring.cloud.service.gemfire.GemfireServiceConnectorConfig;
 
 import org.apache.geode.cache.Region;
 import org.apache.geode.cache.client.ClientCache;
-import org.apache.geode.cache.client.ClientCacheFactory;
 import org.apache.geode.cache.client.ClientRegionFactory;
 import org.apache.geode.cache.client.ClientRegionShortcut;
 import org.apache.geode.pdx.ReflectionBasedAutoSerializer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.Cloud;
+import org.springframework.cloud.CloudFactory;
+import org.springframework.cloud.config.java.AbstractCloudConfig;
+import org.springframework.cloud.service.ServiceConnectorConfig;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.gemfire.support.GemfireCacheManager;
 
 @Configuration
 @EnableCaching
-public class DemoConfig {
+public class DemoConfig extends AbstractCloudConfig {
 	
-//	public ServiceConnectorConfig createGemfireConnectorConfig() {
-//
-//        GemfireServiceConnectorConfig gemfireConfig = new GemfireServiceConnectorConfig();
-//        gemfireConfig.setPoolSubscriptionEnabled(true);
-//        gemfireConfig.setPdxSerializer(new ReflectionBasedAutoSerializer(".*"));
-//        gemfireConfig.setPdxReadSerialized(false);
-//
-//        return gemfireConfig;
-//    }
-//    
-//	@Bean(name = "gemfireCache")
-//    public ClientCache getGemfireClientCache() throws Exception {		
-//		
-//		Cloud cloud = new CloudFactory().getCloud();
-//		ClientCache clientCache = cloud.getServiceConnector("test-pcc", ClientCache.class,  createGemfireConnectorConfig());
-//
-//        return clientCache;
-//    }
-	
-	
-	@Bean(name = "gemfireCache")
-	public ClientCache clientCache() throws Exception {
-        Properties props = new Properties();
-        props.setProperty("security-client-auth-init", "io.pivotal.auth.ClientAuthInitialize.create");
-        
-        ClientCacheFactory ccf = new ClientCacheFactory(props);
-		
-        List<URI> locatorList = EnvParser.getInstance().getLocators();
-		for (URI locator : locatorList) {
-			ccf.addPoolLocator(locator.getHost(), locator.getPort());
-		}
-
-        ccf.setPdxSerializer(new ReflectionBasedAutoSerializer(".*"));
-		ccf.setPdxReadSerialized(false);
-		ccf.setPoolSubscriptionEnabled(true);
-        
-		ClientCache clientCache = ccf.create();
-
-		return clientCache;
+	@Bean
+	public DataSource dataSource() {
+	    return connectionFactory().dataSource();
 	}
+	
+	public ServiceConnectorConfig createGemfireConnectorConfig() {
+
+        GemfireServiceConnectorConfig gemfireConfig = new GemfireServiceConnectorConfig();
+        gemfireConfig.setPoolSubscriptionEnabled(true);
+        gemfireConfig.setPdxSerializer(new ReflectionBasedAutoSerializer(".*"));
+        gemfireConfig.setPdxReadSerialized(false);
+
+        return gemfireConfig;
+    }
+    
+	@Bean(name = "gemfireCache")
+    public ClientCache getGemfireClientCache() throws Exception {		
+		
+		Cloud cloud = new CloudFactory().getCloud();
+		ClientCache clientCache = cloud.getSingletonServiceConnector(ClientCache.class,  createGemfireConnectorConfig());
+
+        return clientCache;
+    }
 
 
 	@Bean(name = "customer")
--- src/main/java/io/pivotal/domain/Customer.java
+++ src/main/java/io/pivotal/domain/Customer.java
@@ -14,13 +14,9 @@ public class Customer {
 	@Id
 	@javax.persistence.Id
 	private String id;
-
 	private String name;
-
 	private String email;
-
 	private String address;
-
 	private String birthday;
 
 	public Customer() {}
@@ -37,47 +33,37 @@ public class Customer {
 	public String getId() {
 		return id;
 	}
-
 	public void setId(String id) {
 		this.id = id;
 	}
-
 	public String getName() {
 		return name;
 	}
-
 	public void setName(String name) {
 		this.name = name;
 	}
-
 	public String getEmail() {
 		return email;
 	}
-
 	public void setEmail(String email) {
 		this.email = email;
 	}
-
 	public String getAddress() {
 		return address;
 	}
-
 	public void setAddress(String address) {
 		this.address = address;
 	}
-
 	public String getBirthday() {
 		return birthday;
 	}
-
 	public void setBirthday(String birthday) {
 		this.birthday = birthday;
 	}
-
+	
 	@Override
 	public String toString() {
 		return "Customer [id=" + id + ", name=" + name + ", email=" + email
 				+ ", address=" + address + ", birthday=" + birthday + "]";
 	}
-
 }
